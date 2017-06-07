d0491be02f6f294f91e9db9527c82e3eb88a8b65
C. Jordan Ball III <cjordanball@gmail.com>
2017-06-06 21:14:06 -0400

    fix index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
+<<<<<<< Updated upstream
 	res.writeHead(200, {'Content-Type': 'text:html'});
+=======
+	res.writeHead(200, {'Content-Type': 'text/html'});
+>>>>>>> Stashed changes
 	res.end('<h1>On the node again!</h1>');
 })
 
